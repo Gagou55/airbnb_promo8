a177aa905593e591c404e2bd10137074d61b3abb
Mickael Giami <mickael.giami@gmail.com>
2015-04-22 11:52:01 +0200

    Reset git

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,47 @@
+class FlatsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def create
+    @flat = current_user.flat.new(flat_params)
+
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def update
+    @flat = current_user.flats.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def edit
+    @flat = current_user.flats.find(params[:id])
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit( :capacity, :street, :zip_code, :city, :description)
+  end
+end
+
+
+
+
+
+

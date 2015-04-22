048136ccc8a0de335440cfe1012d629616e76f07
Mickael Giami <mickael.giami@gmail.com>
2015-04-22 12:11:43 +0200

    reglages seb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421160413) do
+ActiveRecord::Schema.define(version: 20150422100711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 20150421160413) do
     t.datetime "picture_updated_at"
     t.string   "provider"
     t.string   "uid"
-    t.string   "picture"
     t.string   "name"
     t.string   "token"
     t.datetime "token_expiry"

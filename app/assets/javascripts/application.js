5c0a44804ded5d8a4afca314c8011d78cb7be60f
Mickael Giami <mickael.giami@gmail.com>
2015-04-22 23:10:05 +0200

    navbar change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,21 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+  // navbar transition jQuery script
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 0) {
+      $(".navbar").css({
+        "background": "rgba(0, 0, 0, 0.7)",
+        "box-shadow": "0 0 2px black"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+        "box-shadow": "0 0 0px transparent"
+      });
+    }
+  });
+});
